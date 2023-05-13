19f8a0558a88027409f87097ed54857f22eda0a1
Harris Kwong <harriskwong120898@gmail.com>
2023-05-13 11:20:23 -0400

    Set up templates and render out data  to the templates.

--- base/views.py
+++ base/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Home page')
 
-def room(request):
-    return HttpResponse('ROOM')
+rooms = [
+    {'id':1, 'name': 'Liverpool discussion.'},
+    {'id':2, 'name': 'Man City discussion.'},
+    {'id':3, 'name': 'Arsenal discussion.'}
+]
+
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request,'base/home.html',context)
+
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context =  {'room':room}        
+
+    return render(request,'base/room.html',context)
