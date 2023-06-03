375af5cba6ee443ce161001811c3c330c205c51d
Harris Kwong <harriskwong120898@gmail.com>
2023-06-03 15:09:59 -0400

    Modified User database model to include avatars.

--- premierleague/urls.py
+++ premierleague/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.conf import settings
+from django.conf.urls.static import static 
 
 
 urlpatterns = [
@@ -10,3 +10,5 @@ urlpatterns = [
     path('',include('base.urls')),
     path('api/',include('base.api.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
